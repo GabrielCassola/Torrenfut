61bd594ce344ffb96546b702dff6b345403bad03
Gabriel Cassola <cassola.gabriel@gmail.com>
2024-10-09 17:22:12 -0300

    Model CamisetaTamanho

--- venv/torrenfut/store/models.py
+++ venv/torrenfut/store/models.py
@@ -7,7 +7,16 @@ class Camiseta(models.Model):
     cor_principal = models.CharField(max_length=50)
     patrocinador = models.CharField(max_length=100, blank=True, null=True) 
     marca = models.CharField(max_length=100, blank=True, null=True)
-    tamanho = models.CharField(max_length=10)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    quantidade_em_estoque = models.IntegerField()
     imagem = models.ImageField(upload_to='camisetas/', blank=True, null=True)
+
+class CamisetaTamanho(models.Model):
+    TAMANHOS = [
+        ('P', 'Pequeno'),
+        ('M', 'Médio'),
+        ('G', 'Grande'),
+        ('GG', 'Extra Grande'),
+    ]
+    camiseta = models.ForeignKey(Camiseta, related_name='tamanhos', on_delete=models.CASCADE)
+    tamanho = models.CharField(max_length=2, choices=TAMANHOS)
+    quantidade_em_estoque = models.IntegerField()
