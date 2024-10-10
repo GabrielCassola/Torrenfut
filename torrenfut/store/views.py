06eabb5243f84334739b3f248d2a9e32edfcfceb
Gabriel Cassola <cassola.gabriel@gmail.com>
2024-10-10 03:11:43 -0300

    Estoque por tamanho no front

--- torrenfut/store/views.py
+++ torrenfut/store/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, Http404
 from django.template import loader
-from .models import Camiseta
+from .models import Camiseta, CamisetaTamanho
 
 
 def store(request):
@@ -16,11 +16,14 @@ def produto(request, time, estilo, temporada):
     try:
         # Usando get() para buscar uma única camiseta
         camiseta = Camiseta.objects.get(time=time, estilo=estilo, temporada=temporada)
+        tamanhos = CamisetaTamanho.objects.filter(camiseta=camiseta)
     except Camiseta.DoesNotExist:
         raise Http404("Camiseta não encontrada.")
     
     template = loader.get_template('produto.html')
     context = {
-        'camiseta': camiseta,  # Passando um único objeto
+        'camiseta': camiseta, 
+        'tamanhos': tamanhos,
+
     }
     return HttpResponse(template.render(context, request))
