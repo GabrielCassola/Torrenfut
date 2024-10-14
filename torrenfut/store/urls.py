21ea43dd30cfadcc43e4fc6c61c9ee2df0d0794d
Gabriel Cassola <cassola.gabriel@gmail.com>
2024-10-14 11:12:56 -0300

    Arrumando redirect com /

--- torrenfut/store/urls.py
+++ torrenfut/store/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path('store/', views.store, name='store'),
     path('store/produto/<str:time>/<str:estilo>/<str:temporada>', views.produto, name='produto'),
+    re_path(r'^store/produto/(?P<time>[^/]+)/(?P<estilo>[^/]+)/(?P<temporada>.+)$', views.produto, name='produto'),
 ]
 
