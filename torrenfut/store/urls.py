a83ae3fb59aef14401d9d4869228984ccbaf535d
GabrielCassola <cassola.gabriel@gmail.com>
2024-10-31 20:59:07 -0300

    Opções de filtro na home

--- torrenfut/store/urls.py
+++ torrenfut/store/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from .views import filtrar_camisetas
 
 
 urlpatterns = [
     path('store/', views.store, name='store'),
     path('store/produto/<str:time>/<str:estilo>/<str:temporada>', views.produto, name='produto'),
     re_path(r'^store/produto/(?P<time>[^/]+)/(?P<estilo>[^/]+)/(?P<temporada>.+)$', views.produto, name='produto'),
+    path('store/filtrar-camisetas/', filtrar_camisetas, name='filtrar_camisetas'),
 ]
 
